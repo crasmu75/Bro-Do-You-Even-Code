098e1a0225bdc91e2b687d1094a6c824f2ad0c15
crasmu75 <camillerasmussen@live.com>
2015-04-14 08:43:30 -0600

    game and snake classes dun courtesy of Camille and Victor

---
+++ game.js
@@ -0,0 +1,92 @@
+var game = {
+    currentFoodPosition: {x:5, y:5},
+    
+    // How many moves per second we want the snake to do
+    movesPerSec: 2,
+    snakeTimer: undefined,
+    currentScore: 0
+}
+
+startGame();
+
+/*
+ * Resets all game and snake properties
+ */
+function gameOver()
+{
+    // stop the snake moving
+    clearInterval(game.snakeTimer);
+    
+    // reset game properties
+    game.currentFoodPosition = {x:5, y:5};
+    game.movesPerSec = 2;
+    game.snakeTimer = undefined;
+    game.currentScore = 0;
+    
+    // reset snake properties
+    snake.currentDirection = "up";
+    snake.pendingGrowth = false;
+    snake.cells = [
+    {
+        x: 0,
+        y: 0
+    },
+    {
+        x: 0,
+        y: 1
+    },
+    {
+        x: 0,
+        y: 2
+    },
+    {
+        x: 0,
+        y: 3
+    },
+    {
+        x: 0,
+        y: 4
+    }
+    ];
+}
+
+/*
+ * Begins the movement of the snake
+ */
+function startGame()
+{
+    // Start moving the snake every movesPerSec seconds
+    game.snakeTimer = setInterval(function() {
+        if (!snakeMove())
+            gameOver();
+    }, 500);
+}
+
+/*
+ * Generates a random position for the food to appear within the 25 X 25 grid.
+ * However, if the generated position is within the snake cells, a new position is generated.
+ */
+function changeFoodPosition()
+{
+    // get random integer between 0 and 25
+    var randomPos;
+    
+    function generateRandomPos() {
+        randomPos = {x: Math.floor(random() * (26)), y: Math.floor(random() * (26))};
+    }
+    
+    generateRandomPos();
+    
+    // make sure the position is not in the snake
+    // while the position is part of the snake, get another random position
+    
+    function posInSnake(el) {
+        return el.x === randomPos.x && el.y === randomPos.y;
+    }
+    
+    while (snake.cells.some(posInSnake)) {
+        generateRandomPos();
+    }
+    
+    game.currentFoodPosition = randomPos;
+}
---
+++ snake.js
@@ -0,0 +1,84 @@
+var snake = {
+
+    currentDirection: "up",
+    pendingGrowth: false,
+    
+    cells: [
+    {
+        x: 5,
+        y: 16
+    },
+    {
+        x: 5,
+        y: 17
+    },
+    {
+        x: 5,
+        y: 18
+    },
+    {
+        x: 5,
+        y: 19
+    },
+    {
+        x: 5,
+        y: 20
+    }
+    ]
+
+};
+
+/*
+ * Moves the snake by one cell. If the snake has eaten a food, pendingGrowth will be true.
+ * If there is no pendingGrowth, then the tail cell will pop off.
+ */
+function snakeMove()
+{
+    var newHeadCell;
+    
+//    console.log('current head:', snake.cells[0]);
+    switch(snake.currentDirection) {
+        case "up":
+            newHeadCell = {x:snake.cells[0].x, y:snake.cells[0].y - 1};
+            break;
+        case "down":
+            newHeadCell = {x:snake.cells[0].x, y:snake.cells[0].y + 1};
+            break;
+        case "right":
+            newHeadCell = {x:snake.cells[0].x + 1, y:snake.cells[0].y};
+            break;
+        case "left":
+            newHeadCell = {x:snake.cells[0].x - 1, y:snake.cells[0].y};
+            break;
+    }
+    
+    function posInSnake(el) {
+        return el.x === newHeadCell.x && el.y === newHeadCell.y;
+    }
+    
+    // check if this move is valid
+    if(newHeadCell.x < 0 || newHeadCell.x > 24 ||
+      newHeadCell.y < 0 || newHeadCell.y > 24 ||
+      snake.cells.some(posInSnake))
+        return false;
+
+    snake.cells.splice(0, 0, newHeadCell);
+
+    if(!snake.pendingGrowth)
+        // Pop tail cell off
+        snake.cells.pop();
+    else
+        // Don't pop off tail cell, but reset pendingGrowth
+        snake.pendingGrowth = false;
+    
+    return true;
+}
+
+/*
+ * pendingGrowth is set to true, so the next time snakeMove is called, 
+ * the snake will grow.
+ */
+function snakeEat()
+{
+    snake.pendingGrowth = true;
+}
